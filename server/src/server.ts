3ee9fbbc79ad559ff3d48049b83819c1daa38dab
Ikraam <IbnDaanis@Gmail.com>
2021-04-18 23:14:41 +0100

    More setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
 import { config } from './config/config'
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+} else {
+  app.get('/', (_, res) => {
+    res.send('API is running')
+  })
+}
 
 app.listen(config.server, () => {
   console.log(`Server running on ${config.server.hostname}:${config.server.port}`)
