9baa880a9a8e63ac02b423590a1d7d759e1ad342
Ikraam <IbnDaanis@Gmail.com>
2021-04-20 03:08:28 +0100

    Refactored item controller

--- server/src/controllers/itemsControllers.ts
+++ server/src/controllers/itemsControllers.ts
@@ -1,12 +1,9 @@
-import { query, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { pool } from '../config/database'
 
 export const getItems = (req: Request, res: Response) => {
   pool.query('SELECT * FROM item', (error, results) => {
-    if (error) {
-      return res.send(error.message)
-    }
-    console.log(results.rows)
+    if (error) return res.send(error.message)
     res.json(results.rows)
   })
 }
@@ -14,18 +11,13 @@ export const getItems = (req: Request, res: Response) => {
 export const addItem = async (req: Request, res: Response) => {
   const { name, description, price, category, url, stock } = req.body
   const item = await pool.query('SELECT * FROM ITEM WHERE name=$1', [name])
-  if (item) {
-    return res.status(400).json('This item already exists, choose a different name')
-  }
+  if (item) return res.status(400).json('This item already exists, choose a different name')
 
   pool.query(
     'INSERT INTO item(name, description, price, category, url, stock) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
     [name, description, price, category, url, stock],
     (error, results) => {
-      if (error) {
-        return res.send(error.message)
-      }
-      console.log(results.rows)
+      if (error) return res.send(error.message)
       res.json(results.rows)
     }
   )
