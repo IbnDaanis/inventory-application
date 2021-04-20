c8f8655676af04e2e2d1fc2cb73a98eec76822a9
Ikraam <IbnDaanis@Gmail.com>
2021-04-20 01:40:09 +0100

    item table

--- server/src/config/database.sql
+++ server/src/config/database.sql
@@ -1,6 +1,18 @@
 CREATE DATABASE inventory
 
 CREATE TABLE category(
-  category_id SERIAL PRIMARY KEY,
-  title VARCHAR(100)
+  category_id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(100) NOT NULL
 )
+
+CREATE TABLE item(
+  item_id SERIAL PRIMARY KEY NOT NULL,
+  description TEXT NOT NULL,
+  price INT NOT NULL,
+  stock INT NOT NULL,
+  item_url VARCHAR(300) NOT NULL,
+  item_name VARCHAR(100) NOT NULL,
+  category INT NOT NULL,
+  FOREIGN KEY (category) REFERENCES category(category_id)
+)
+
