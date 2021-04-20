50c7ba4b93aa9ce15c9baa7b6118cfe2bd63734a
Ikraam <IbnDaanis@Gmail.com>
2021-04-20 23:59:25 +0100

    Fetched data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+interface CategoryInterface {
+  category_id: string
+  title: string
+  description: string
+}
+
 export const App = () => {
+  const [categories, setCategories] = useState<CategoryInterface[]>([])
+
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('http://localhost:5000/api/items')
+      const { data } = await axios.get('http://localhost:5000/api/categories')
       console.log(data)
+      setCategories(data)
     }
     getData()
   }, [])
 
-  return <div className="App">Hello</div>
+  return (
+    <div className="App">
+      Hello
+      <div className="categories">
+        {categories?.map((category) => (
+          <h2 key={category.category_id}>{category?.title}</h2>
+        ))}
+      </div>
+    </div>
+  )
 }
