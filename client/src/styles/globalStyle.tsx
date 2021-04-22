ccf439fae9e45455edaf2ced9082997801da538c
Ikraam <IbnDaanis@Gmail.com>
2021-04-23 00:23:20 +0100

    Added styled components

---
+++ client/src/styles/globalStyle.tsx
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+a {
+  text-decoration: none;
+  color: unset;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-weight: 500;
+}
+input {
+  -webkit-border-radius: 0;
+  border-radius: 0;
+  font-size: 16px;
+}
+
+ul{
+  list-style: none;
+}
+
+select {
+  font-size: 16px;
+}
+
+label {
+  font-size: 14px ;
+}
+
+.no-select {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.hide {
+  display: none !important;
+}
+`
